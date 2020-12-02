175c96a72f84978b8310decd71a88f1b8c6cdffd
bilal.alnaani@fh-erfurt.de <bilal.alnaani@fh-erfurt.de>
2020-12-02 12:55:28 +0100

    the first Recipe Design closes#4

--- app/src/main/java/de/fherfurt/onlyoneegg/recipe/RecipeViewModel.kt
+++ app/src/main/java/de/fherfurt/onlyoneegg/recipe/RecipeViewModel.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.AndroidViewModel
 
 class RecipeViewModel (application: Application): AndroidViewModel(application) {
 
-   // val recipe ="test"
+    val recipeName ="Greek Chicken Skewers"
+
+    val recipe ="Whisk lemon juice, oil, vinegar, onion flakes, garlic, lemon zest, Greek seasoning," +
+            " poultry seasoning, oregano, pepper, and thyme together in a bowl and pour into a resealable plastic bag."
 
 }
