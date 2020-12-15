ec11a9c22437f2f46c8dc1b21bb81da5d07085e5
FranziskaSchm <fr_schm@web.de>
2020-12-15 16:54:49 +0100

    update cookbook to database

--- app/src/main/java/de/fherfurt/onlyoneegg/view/ui/cookbook/CookBookFragment.kt
+++ app/src/main/java/de/fherfurt/onlyoneegg/view/ui/cookbook/CookBookFragment.kt
@@ -7,34 +7,49 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+
+import androidx.recyclerview.widget.GridLayoutManager
 import de.fherfurt.onlyoneegg.R
 import de.fherfurt.onlyoneegg.databinding.FragmentCookbookBinding
+import de.fherfurt.onlyoneegg.storage.OOEDatabase
+import de.fherfurt.onlyoneegg.storage.RecipeRepository
 
 
 class CookBookFragment : Fragment() {
 
-
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
 
         // Get a reference to the binding object and inflate the fragment views.
         val binding : FragmentCookbookBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_cookbook, container, false)
 
-
         val application = requireNotNull(this.activity).application
 
-
-        val viewModelFactory = CookBookViewModelFactory(application)
+        val recipeDao = OOEDatabase.getInstance(application).recipeDao
+        val recipeRepository= RecipeRepository(recipeDao)
+        val viewModelFactory = CookBookViewModelFactory(application, recipeRepository)
 
         val cookbookViewModel =
                 ViewModelProvider(
                         this, viewModelFactory).get(CookBookViewModel::class.java)
 
         binding.cookbookViewModel = cookbookViewModel
+       /** binding.addRecipe.setOnClickListener { findNavController().navigate(R.id.action_cookbookFragment_to_inputrecipeFragment) } **/
         binding.setLifecycleOwner(this)
+
+        val adapter  = CookBookAdapter()
+        binding.recipeList.adapter = adapter
+
+        cookbookViewModel.recipes.observe(viewLifecycleOwner, {
+            it?.let{
+                adapter.submitList(it)
+            }
+        })
+        val manager = GridLayoutManager(activity, 2, GridLayoutManager.VERTICAL, false)
+        binding.recipeList.layoutManager = manager
+
         return binding.root
 
 
--- app/src/main/java/de/fherfurt/onlyoneegg/view/ui/cookbook/CookBookViewModelFactory.kt
+++ app/src/main/java/de/fherfurt/onlyoneegg/view/ui/cookbook/CookBookViewModelFactory.kt
@@ -3,12 +3,16 @@ package de.fherfurt.onlyoneegg.view.ui.cookbook
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import de.fherfurt.onlyoneegg.storage.RecipeRepository
 
-class CookBookViewModelFactory (private val application: Application) : ViewModelProvider.Factory {
+
+class CookBookViewModelFactory (
+    private val application: Application,
+    private val dataSource: RecipeRepository) : ViewModelProvider.Factory {
     @Suppress("unchecked_cast")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(CookBookViewModel::class.java)) {
-            return CookBookViewModel(application) as T
+            return CookBookViewModel(application, dataSource) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
